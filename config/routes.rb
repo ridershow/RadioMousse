a03df139b62ab5d67a2d7c4d65093deb3786c103
jeremy lanfranchi <lanfranchi.jeremy@gmail.com>
2018-09-27 16:07:35 +0200

    create amdin js in asset pipeline and removed admin js from webbpacker

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, only: %i[new create index]
 end
 
-resources :songs, only: %i[new create index]

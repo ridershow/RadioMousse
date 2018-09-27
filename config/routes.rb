00383b7a50706f0cb45f9b25b4afc9efc0f484b4
jeremy lanfranchi <lanfranchi.jeremy@gmail.com>
2018-09-27 15:15:37 +0200

    Installed new gem and configure the first assets of the web app

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+resources :songs, only: %i[new create index]
